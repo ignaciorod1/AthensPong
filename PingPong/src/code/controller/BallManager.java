6ba00a655713199712e218e8858440692b92ff26
Juliauru <37532390+Juliauru@users.noreply.github.com>
2018-03-20 10:38:17 +0100

    Update BallManager.java

--- PingPong/src/code/controller/BallManager.java
+++ PingPong/src/code/controller/BallManager.java
@@ -1,4 +1,5 @@
 package code.controller;
+package model;
 
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
@@ -19,4 +20,18 @@ public class BallManager implements ActionListener, KeyListener{
     public BallManager(){
         ball = new Ball()
     }
+    
+    public int checkCollision(Paddle paddle)
+	{
+		if (this.x < paddle.x + paddle.width && this.x + width > paddle.x && this.y < paddle.y + paddle.height && this.y + height > paddle.y)
+		{
+			return 1; //bounce
+		}
+		else if ((paddle.x > x && paddle.paddleNumber == 1) || (paddle.x < x - width && paddle.paddleNumber == 2))
+		{
+			return 2; //score
+		}
+
+		return 0; //nothing
+	}
 }
