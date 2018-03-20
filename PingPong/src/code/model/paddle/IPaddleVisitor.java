96287e645b1d9750781c04f453ebe6607c8b578a
Unknown <kpb15148@uni.strath.ac.uk>
2018-03-20 10:30:35 +0100

    Delete paddle and pong dir

--- PingPong/src/code/model/paddle/IPaddleVisitor.java
+++
@@ -1,6 +0,0 @@
-package code.model.paddle;
-
-public interface IPaddleVisitor {
-	public void handlePlayerOne(PlayerOne player);
-	public void handlePlayerTwo(PlayerTwo player);
-}
--- PingPong/src/code/model/paddle/PlayerTwo.java
+++
@@ -1,9 +0,0 @@
-package code.model.paddle;
-
-public class PlayerTwo extends APaddle{
-
-	@Override
-	public void AcceptVisitor(IPaddleVisitor visitor) {
-		visitor.handlePlayerTwo(this);
-	}
-}
